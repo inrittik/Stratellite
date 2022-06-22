c13cb787282cf3a4ecf4ace70d44aefd95355aac
Nrittik Sarmah <nrittik43@gmail.com>
2022-06-22 15:15:27 +0530

    bidding page completed

--- src/Pages/Projects/Sections/Bidding.tsx
+++ src/Pages/Projects/Sections/Bidding.tsx
@@ -10,42 +10,44 @@ const Bidding = () => {
         <div className="mr-3">{icons.bidding.plot}</div>
         Plot No. 123, New Delhi, India, 152546
       </div>
-      <div className="flex items-center py-2 rounded-md cursor-pointer text-sm text-center md:text-xssm mx-3 md:mx-0 text-red-500">
+      <div className="flex items-center py-2 rounded-md cursor-pointer text-xs text-center md:text-sm text-red-500">
         <div className="mr-3">{icons.bidding.clock}</div>
         Submit before 22nd June, 2022
       </div>
 
-      <div className="flex flex-col my-3">
-        <div className="flex items-center mb-3">
+      <div className="flex flex-col my-3 mt-8">
+        <div className="flex items-center mb-5">
           <div className="p-3 rounded w-12 h-12 flex justify-center items-center cursor-pointer text-gray-100 border border-slate-300 mr-3">
             {icons.bidding.done}
           </div>
           <div className="flex flex-col">
-            <div className="font-semibold text-base">
+            <div className="font-semibold text-sm md:text-base mb-1">
               Quote Range - $650 to $830
             </div>
-            <div className="font-semibold text-xssm">10th Nov, 2021</div>
+            <div className="text-xssm">10th Nov, 2021</div>
           </div>
         </div>
 
-        <div className="flex items-center mb-3">
+        <div className="flex items-center mb-5">
           <div className="p-3 rounded w-12 h-12 flex justify-center items-center cursor-pointer text-gray-100 border border-slate-300 mr-3">
             {icons.bidding.done}
           </div>
           <div className="flex flex-col">
-            <div className="font-semibold text-base">
+            <div className="font-semibold text-sm md:text-base mb-1">
               Enter Final Quote Amount
             </div>
-            <div className="font-semibold text-xssm">10th Dec, 2021</div>
+            <div className="-xssm">10th Dec, 2021</div>
           </div>
         </div>
 
-        <div className="flex items-center mb-3">
+        <div className="flex items-center mb-5">
           <div className="p-3 rounded w-12 h-12 flex justify-center items-center cursor-pointer text-gray-100 border border-slate-300 mr-3">
             <div className="border-2 w-5 h-5 rounded-full border-black"></div>
           </div>
           <div className="flex flex-col">
-            <div className="font-semibold text-base">Quote Status</div>
+            <div className="font-semibold text-sm md:text-base mb-1">
+              Quote Status
+            </div>
           </div>
         </div>
       </div>
@@ -54,42 +56,55 @@ const Bidding = () => {
         name=""
         id=""
         cols={30}
-        rows={10}
-        className="border border-slate-300 focus:outline-none rounded my-5 p-5 placeholder-gray-700 text-gray-700"
+        rows={8}
+        className="border border-slate-300 focus:outline-none rounded my-3 p-5 text-sm  placeholder-gray-700 text-gray-700"
         placeholder="Write a message......"
       ></textarea>
 
-      <div className="border border-slate-300 flex items-center my-5">
+      <div className="border border-slate-300 flex items-center my-3">
         <label htmlFor="quoteprice" className="w-7 p-3 text-gray-100">
           {icons.Projects.price}
         </label>
         <input
           type="text"
-          className="quoteprice outline-none rounded p-5 placeholder-gray-700 text-gray-700 w-11/12"
+          className="quoteprice outline-none rounded p-5 placeholder-gray-700 placeholder-gray-700 text-sm text-gray-700 w-11/12"
           placeholder={`Quote Amount`}
         />
       </div>
 
-      <div className="border border-slate-300 border-dashed flex items-center my-5 px-3 py-8">
-        <label htmlFor="quoteprice" className="w-7 text-gray-100">
-          {icons.bidding.file}
+      <div className="border border-slate-300 border-dashed flex items-center justify-between my-3 px-3 py-4">
+        <div className="flex items-center justify-between">
+          <label htmlFor="quoteprice" className="w-7 text-gray-100">
+            {icons.bidding.file}
+          </label>
+          <div className="text-gray-900 ml-3 text-sm">Attach File</div>
+        </div>
+
+        <label htmlFor="file-upload">
+          <div className="flex items-center font-semibold justify-between py-3 text-sky-400 border-sky-400 border rounded text-white cursor-pointer w-40 px-5 text-sm text-center">
+            {icons.bidding.upload} Upload File
+          </div>
         </label>
-        <div className="text-gray-700 ml-3">Attach File</div>
+        <input type="file" id="file-upload" className="hidden" />
       </div>
 
-      <div className="flex my-5">
-        <div className="flex items-center mr-6">
-          <input type="radio" />
+      <div className="flex flex-col md:flex-row my-5">
+        <div className="flex items-center text-sm mr-10 mb-4 md:mb-0">
+          <input type="radio" className="h-6 w-6 text-sky-400" />
           <label htmlFor="" className="ml-3">
             I Accept Payment Terms
           </label>
         </div>
-        <div className="flex items-center">
-          <input type="radio" />
+        <div className="flex items-center text-sm">
+          <input type="radio" className="h-6 w-6 text-sky-400" />
           <label htmlFor="" className="ml-3">
             I Accept Delivery Terms
           </label>
         </div>
+      </div>
+
+      <div className="p-4 bg-sky-400 rounded text-white cursor-pointer md:w-20 text-center text-sm my-5 md:p-3">
+        Submit
       </div>
     </div>
   );
