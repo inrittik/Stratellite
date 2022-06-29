c9e8ba95490ee5e685b412bfcc18c22c2abc0209
Nrittik Sarmah <nrittik43@gmail.com>
2022-06-29 10:49:05 +0530

    design for photo gallery completed

--- src/Pages/Projects/Sections/PhotoGallery.tsx
+++ src/Pages/Projects/Sections/PhotoGallery.tsx
@@ -1,9 +1,12 @@
 import GallerySection from "../../../Components/GallerySection";
+import { graphics } from "../../../utils/graphics";
 
 const PhotoGallery = () => {
   return (
     <div className="flex flex-col md:w-10/12 mt-6 md:mt-0 ml-6 md:ml-12 overflow-y-auto">
-      <div className="flex justify-between items-center mb-4">
+      {/* when images are posted */}
+
+      {/* <div className="flex justify-between items-center mb-4">
         <div className="w-2/3 md:w-fit">
           <div className="text-base md:text-1.5xl text-gray-900 font-semibold my-2">
             Work Photos
@@ -15,12 +18,29 @@ const PhotoGallery = () => {
         <div className="flex items-center h-fit py-3 px-2 mr-2 md:px-4 font-semibold border rounded border-sky-400 cursor-pointer text-center text-sky-400 md:mr-0">
           Upload Images
         </div>
-      </div>
+      </div> */}
 
-      {/* section for images */}
+      {/* <GallerySection />
       <GallerySection />
-      <GallerySection />
-      <GallerySection />
+      <GallerySection /> */}
+
+      {/* when no images are posted */}
+
+      <div className="flex flex-col items-center justify-center my-20 md:my-0">
+        <div className="h-1/2 w-1/2">{graphics.gallery.post}</div>
+
+        <div className="flex flex-col text-center items-center justify-center my-4">
+          <div className="text-base md:text-1.5xl text-gray-900 font-semibold my-2">
+            Start Posting
+          </div>
+          <div className="text-xs md:text-sm text-gray-700">
+            Start Posting images of your daily work
+          </div>
+          <div className="flex items-center w-fit h-fit py-3 px-2 mr-2 md:px-4 font-semibold border rounded border-sky-400 cursor-pointer text-center text-sky-400 my-3 md:mr-0">
+            Upload Images
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
